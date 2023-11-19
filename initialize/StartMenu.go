e447fd68bef099c6915205b66e7b984e4a9b27ac
babyboydaprince <bala_zxz@hotmail.com>
2023-11-18 21:20:09 -0300

    base project

---
+++ initialize/StartMenu.go
@@ -0,0 +1,30 @@
+package initialize
+
+import (
+	"fmt"
+	"github.com/nexidian/gocliselect"
+)
+
+func Banner() {
+	asciiLogo1 := figure.NewColorFigure("Go change", "Bloody", "red", true)
+	asciiLogo2 := figure.NewColorFigure("my mac!", "Bloody", "blue", true)
+
+	asciiLogo1.Print()
+	asciiLogo2.Print()
+}
+
+func Main() {
+
+	menu := gocliselect.NewMenu("Chose a colour")
+
+	menu.AddItem("Red", "red")
+	menu.AddItem("Blue", "blue")
+	menu.AddItem("Green", "green")
+	menu.AddItem("Yellow", "yellow")
+	menu.AddItem("Cyan", "cyan")
+
+	choice := menu.Display()
+
+	fmt.Printf("Choice: %s\n", choice)
+
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package rockitforme
+
+import (
+	"github.com/babyboydaprince/rockitforme/initialize"
+)
+
+func main() {
+	for {
+		initialize.Banner()
+	}
+}

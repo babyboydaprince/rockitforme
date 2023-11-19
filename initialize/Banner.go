7641f03fc534d5814acc1af0aa84266f6b56ece4
babyboydaprince <bala_zxz@hotmail.com>
2023-11-19 03:49:23 -0300

    (add) banner module and interactive start menu

--- initialize/Banner.go
+++ initialize/Banner.go
@@ -4,8 +4,8 @@ import "github.com/common-nighthawk/go-figure"
 
 func Banner() {
 
-	asciiLogo1 := figure.NewColorFigure("Go change", "slant", "cyan", true)
-	asciiLogo2 := figure.NewColorFigure("my mac!", "slant", "yellow", true)
+	asciiLogo1 := figure.NewColorFigure("Rock it", "Bloody", "red", true)
+	asciiLogo2 := figure.NewColorFigure("Fotr me!", "Bloody", "blue", true)
 
 	asciiLogo1.Print()
 	asciiLogo2.Print()

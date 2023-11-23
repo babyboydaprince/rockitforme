46751c02f2c538d9ddfd8d687df3979a801f1ae8
babyboydaprince <bala_zxz@hotmail.com>
2023-11-23 01:47:01 -0300

    (add) more tools to the menu

--- initialize/start_menu.go
+++ initialize/start_menu.go
@@ -8,23 +8,50 @@ import (
 	"rockitforme/banner"
 	"rockitforme/common"
 	"rockitforme/tools"
-	"sync"
 	"syscall"
-	"time"
 )
+
+var inSubmodule bool
 
 func StartMenu() {
 
+	inSubmodule = false
+
+	// Listen for Ctrl+C signal
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+
+	// Go routine to handle Ctrl+C signal
+	go func() {
+		<-sigCh
+
+		fmt.Print("\033[H\033[2J") // Clear the console
+		banner.PrintBanner()
+
+		if inSubmodule {
+			fmt.Print("\n\nReturning to the main menu...\n\nHit ENTER!")
+
+		} else {
+			fmt.Print("\n\nExiting...")
+			os.Exit(0)
+		}
+	}()
+
 MainMenuLoop:
+
 	for {
-		options := common.SingleSelect("\n       Made by:  BraiNiac\n", []string{
-			"Net tools",
-			"Web",
-			"Wireless",
-			"SDR",
-			"NFC & RFID",
-			"Exit",
-		})
+
+		options := common.SingleSelect(
+			"\n     ---THE EVER PRESENT---\n    ----SILENT OBSERVER-----\n\n       Made by:  BraiNiac\n\n", []string{
+				"Net tools",
+				"Web",
+				"Wireless",
+				"SDR",
+				"NFC & RFID",
+				"Exit",
+			})
+
+		inSubmodule = false
 
 		if options == "" {
 			// If the user presses Enter without choosing an option, stay in the current menu
@@ -35,10 +62,12 @@ MainMenuLoop:
 
 		case "Net tools":
 
-			fmt.Print("\033[H\033[2J") // Clear the console
-			banner.PrintBanner()
+			inSubmodule = false
 
-			netOptions := common.SingleSelect("\n       ----NET TOOLS----\n", []string{
+			fmt.Print("\033[H\033[2J") // Clear the console
+			banner.BannerNetTools()
+
+			netOptions := common.SingleSelect("\n  ----TO SHOW YOU THE WAY----\n", []string{
 				"MAC Changer",
 				"Port scanner",
 				"Trace ip route",
@@ -55,43 +84,35 @@ MainMenuLoop:
 			switch netOptions {
 
 			case "MAC Changer":
+
+				inSubmodule = false
+
 				tools.GoChangeMyMac()
 
 			case "Port scanner":
-				fmt.Print("\033[H\033[2J") // Clear the console
-				banner.PrintBanner()
 
-				scanner := bufio.NewScanner(os.Stdin)
+				inSubmodule = false
 
-				fmt.Print("\nInsert URL or IP ADDRESS to scan: ")
-
-				scanner.Scan()
-
-				input := scanner.Text()
-
-				tools.PortScan(input)
+				tools.PortScan()
 
 			case "Trace ip route":
-				fmt.Print("\033[H\033[2J") // Clear the console
-				banner.PrintBanner()
 
-				scanner := bufio.NewScanner(os.Stdin)
+				inSubmodule = false
 
-				fmt.Print("\nInsert URL or IP ADDRESS to trace the route ")
-
-				scanner.Scan()
-
-				input := scanner.Text()
-
-				tools.TraceRoute(input)
+				tools.TraceRoute()
 
 			case "Arp spoof":
+
+				inSubmodule = false
+
 				fmt.Print("\033[H\033[2J") // Clear the console
-				banner.PrintBanner()
+				banner.BannerArpSpoof()
+
+				fmt.Print("\n    ----TO KEEP WATCH---\n")
 
 				scanner := bufio.NewScanner(os.Stdin)
 
-				fmt.Print("\nSet TARGET IP to spoof: ")
+				fmt.Print("\n\nSet TARGET IP to spoof: ")
 
 				scanner.Scan()
 
@@ -103,38 +124,7 @@ MainMenuLoop:
 
 				gateway := scanner.Text()
 
-				// Channel to receive Ctrl+C signal
-				sigChan := make(chan os.Signal, 1)
-				signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-
-				// Channel to notify the ARP spoofing goroutine to stop
-				stopChan := make(chan struct{})
-
-				// Wait group for synchronization
-				var wg sync.WaitGroup
-
-				// Start ARP spoofing command in the background
-				wg.Add(1)
-				go func() {
-					defer wg.Done()
-					tools.ArpSpoof(target, gateway, stopChan)
-				}()
-
-				// Wait for Ctrl+C signal or ARP spoofing completion
-				select {
-				case <-sigChan:
-					// If Ctrl+C is pressed, interrupt ARP spoofing
-					fmt.Println("\nARP Spoofing interrupted. Returning to the main menu.")
-					stopChan <- struct{}{} // Notify the ARP spoofing to stop
-				case <-time.After(5 * time.Second): // Adjust the duration based on your needs
-					// After a certain duration, assume the ARP spoofing command has completed
-					fmt.Println("\nARP Spoofing completed!")
-					time.Sleep(2 * time.Second)
-
-				}
-
-				// Wait for the ARP spoofing goroutine to finish
-				wg.Wait()
+				tools.ArpSpoof(target, gateway)
 
 			case "Go back":
 				fmt.Print("\033[H\033[2J") // Clear the console

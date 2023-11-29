70a9ed375231b38e1f8ce64bde6ee57f4c2f50b5
babyboydaprince <bala_zxz@hotmail.com>
2023-11-28 23:06:09 -0300

    (add) run with root check

--- main.go
+++ main.go
@@ -1,11 +1,42 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"rockitforme/banner"
 	"rockitforme/initialize"
+	"runtime"
 )
 
 func main() {
+
+	if !isRootOrAdmin() {
+		banner.PrintBanner()
+		fmt.Println("\n\nIt seems like you are not all that ready to rock...\n\n" +
+			"Must be run as root (Unix) or administrator (Windows). \n\nExiting...")
+		os.Exit(1)
+	}
+
 	banner.PrintBanner()
 	initialize.StartMenu()
+}
+
+func isRootOrAdmin() bool {
+	switch runtime.GOOS {
+	case "windows":
+		return isAdminWindows()
+	case "linux", "darwin":
+		return isRootUnix()
+	default:
+		return false
+	}
+}
+
+func isAdminWindows() bool {
+	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
+	return err == nil
+}
+
+func isRootUnix() bool {
+	return os.Geteuid() == 0
 }
